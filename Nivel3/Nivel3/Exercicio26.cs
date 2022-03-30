111a605016bfe3f60b9a0ee95c2a4ab5b83d357a
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-03-30 16:52:52 -0300

    Primeira comitada

---
+++ Nivel3/Nivel3/Exercicio26.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Nivel3
+{
+    public class Exercicio26
+    {
+        static void Main1(string[] args)
+        {
+            for(int i = 1; i <= 100 ; i++)
+            {
+                Console.WriteLine("º{0} Número",i);
+            }
+        }
+    }
+   
+}
---
+++ Nivel3/Nivel3/Exercicio27.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nivel3
+{
+    internal class Exercicio27
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 0; i++)
+            {
+
+            }
+        }
+    }
+}
