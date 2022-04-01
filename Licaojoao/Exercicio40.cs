b3260d774d35fd736b5d5d6be085946f484c9129
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-04-01 17:21:35 -0300

    Não acabei to f*****

---
+++ Licaojoao/Exercicio40.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Licaojoao
+{
+    class Exercicio40
+    {
+        static void Main18(string[] args)
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                Console.WriteLine("º{0} Número", i);
+            }
+        }
+    }
+}
--- Licaojoao/Exercicio41.cs
+++ Licaojoao/Exercicio41.cs
@@ -4,12 +4,34 @@ namespace Licaojoao
 {
     class Exercicio41
     {
-        static void Main16(string[] args)
+        static void Main17(string[] args)
         {
-            for (int i = 1; i <= 100; i++)
+
+            int toReceiveNumber = 0;
+            Console.WriteLine("Digite a idade do nadador");
+            toReceiveNumber = Convert.ToInt32(Console.ReadLine());
+            if (toReceiveNumber >= 5 && toReceiveNumber <= 7)
             {
-                Console.WriteLine("º{0} Número", i);
+                Console.WriteLine("Infantil A = 5 a 7 anos ");
             }
+            else if (toReceiveNumber >= 8 && toReceiveNumber <= 11)
+            {
+                Console.WriteLine("Infantil B = 8 a 11 anos  ");
+            }
+            else if (toReceiveNumber >= 12 && toReceiveNumber <= 13)
+            {
+                Console.WriteLine("Juvenil A = 12 a 13 anos  ");
+            }
+            else if (toReceiveNumber >= 14 && toReceiveNumber <= 17)
+            {
+                Console.WriteLine("Juvenil B = 14 a 17 anos");
+            }
+            else if (toReceiveNumber >= 18)
+            {
+                Console.WriteLine("Adultos = Maiores de 18 anos ");
+            }
+
+
         }
     }
 }
