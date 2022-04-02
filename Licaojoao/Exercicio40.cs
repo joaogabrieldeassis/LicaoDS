7eabc096d611bb810b6c06ce20f217bba3569951
joaogabrieldeassis <joaoassisgabriel@gmail.com>
2022-04-01 23:48:44 -0300

    Subindo40

--- Licaojoao/Exercicio40.cs
+++ Licaojoao/Exercicio40.cs
@@ -1,14 +1,38 @@
 using System;
-
-namespace Licaojoao
+namespace joao
 {
-    class Exercicio40
+    class joao
     {
-        static void Main18(string[] args)
+        static void Main15(string[] args)
         {
-            for (int i = 1; i <= 100; i++)
+            double primaryNumber = 0;
+            char receiveCharacter;
+            while (true)
             {
-                Console.WriteLine("º{0} Número", i);
+                Console.WriteLine("Digite o indice da sua empresa com ',' \n");
+                primaryNumber = Convert.ToDouble(Console.ReadLine());
+                if (primaryNumber <= 0.25)
+                {
+                    Console.WriteLine("Sua empresa está dentro dos regurlamentos:\n ");
+                }
+                if (primaryNumber == 0.3)
+                {
+                    Console.WriteLine("ATENÇÃO TODAS AS INDUSTRIAS DO PRIMEIRO GRUPO ESTÃO SUSPENSAS:\n");
+                }
+                if (primaryNumber == 0.4)
+                {
+                    Console.WriteLine("ATENÇÃO TODAS AS INDUSTRIAS DO PRIMEIRO E SEGUNDO GRUPO ESTÃO SUSPENSAS\nSuas atividades:\n");
+                }
+                if (primaryNumber == 0.5)
+                {
+                    Console.WriteLine("ATENÇÃO IMEDIATO TODAS AS INDUSTRIAS DE TODOS OS GRUPOS ESTÃO SUSPENSAS:\n");
+                }
+                Console.WriteLine("Deseja sair do programa S para sim N para não:");
+                receiveCharacter = Convert.ToChar(Console.ReadLine());
+                if (receiveCharacter == 's' || receiveCharacter == 'S')
+                {
+                    break;
+                }
             }
         }
     }
